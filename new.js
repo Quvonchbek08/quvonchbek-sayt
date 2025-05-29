adb12835779a5606887ad0f28f4ae89979bb7104
Quvonchbek08 <quvonchbekjoraboyev11@gmail.com>
2025-05-29 20:37:49 +0300

    Add files via upload

---
+++ new.js
@@ -0,0 +1,16 @@
+document.querySelector("form").addEventListener("submit", function (e) { alert("Xabaringiz yuborildi! Rahmat."); });
+window.onload = function () { alert("Saytimga xush kelibsiz, Quvonchbek!"); };
+setInterval(function) {
+    const now = new Date();
+    document.getElementById("clock").textContent = now.toLocaleDateString();
+}, 1000;
+
+
+
+
+
+
+
+
+
+
